2eee39fb33448e6247474367867e9f80e697eea9
ArtdragonXoX <1262922587@qq.com>
2024-12-15 14:44:36 +0800

    初始化

--- main.go
+++ main.go
@@ -1,79 +1,36 @@
-/*
-@author Howard Zheng <hex9cf@aliyun.com>
-*/
 package main
 
 import (
-	"bufio"
-	"cupid-connector/api"
-	"cupid-connector/conf"
-	"fmt"
-	"log"
-	"os"
-	"strings"
+	"embed"
+
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
 )
 
-const rememberPwd = "0"
+//go:embed all:frontend/dist
+var assets embed.FS
 
 func main() {
-	log.SetOutput(os.Stdout)
-	err := conf.InitEnv()
-	if err != nil {
-		panic(err)
-	}
+	// Create an instance of the app structure
+	app := NewApp()
 
-	err = api.Login(conf.Url, conf.Username, conf.Password, rememberPwd)
-	if err != nil {
-		log.Println(err)
-	}
-	if err == nil && conf.AutoExit == "TRUE" {
-		os.Exit(0)
-	}
+	// Create application with options
+	err := wails.Run(&options.App{
+		Title:  "CupidConnector",
+		Width:  1024,
+		Height: 768,
+		AssetServer: &assetserver.Options{
+			Assets: assets,
+		},
+		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		OnStartup:        app.startup,
+		Bind: []interface{}{
+			app,
+		},
+	})
 
-	reader := bufio.NewReader(os.Stdin)
-	for {
-		fmt.Println("╔═════════════════════════╗")
-		fmt.Println("║     Cupid Connector     ║")
-		fmt.Println("╠═════════════════════════╣")
-		fmt.Println("║ 退出程序：exit          ║")
-		fmt.Println("║ 重新认证：login         ║")
-		fmt.Println("║ 注销账号：logout        ║")
-		fmt.Println("║ 初始化配置：init        ║")
-		fmt.Println("╚═════════════════════════╝")
-		for reader.Buffered() > 0 {
-			_, err := reader.Discard(reader.Buffered())
-			if err != nil {
-				log.Println(err)
-				break
-			}
-		}
-		fmt.Print("> ")
-		input, _ := reader.ReadString('\n')
-		input = strings.TrimSpace(input)
-		input = strings.ToLower(input)
-		switch input {
-		case "logout":
-			err := api.Logout(conf.Url)
-			if err != nil {
-				log.Println(err)
-				break
-			}
-		case "login":
-			err := api.Login(conf.Url, conf.Username, conf.Password, rememberPwd)
-			if err != nil {
-				log.Println(err)
-				break
-			}
-		case "init":
-			err := conf.CreateEnv()
-			if err != nil {
-				log.Println(err)
-				break
-			}
-		case "exit":
-			os.Exit(0)
-		default:
-			fmt.Println("未知命令！")
-		}
+	if err != nil {
+		println("Error:", err.Error())
 	}
 }
